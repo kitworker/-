c289ca7dba4b40f0cd54c75a330c8d58eb79c42e
kitworker <kasyanov@gmail.com>
2016-10-09 22:22:22 +0300

    git

---
+++ дневник.mm
@@ -0,0 +1,6 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1476040447127" ID="ID_697292367" MODIFIED="1476040470912" TEXT="&#x434;&#x43d;&#x435;&#x432;&#x43d;&#x438;&#x43a;">
+<node CREATED="1476040471893" ID="ID_419374764" MODIFIED="1476040506581" POSITION="right" TEXT="git"/>
+</node>
+</map>
